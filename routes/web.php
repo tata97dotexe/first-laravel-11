868c280ca658c07dede5264ff05b3c48da3aac26
Tuahta Ramadhani <tuahtaramadhani@gmail.com>
2025-03-04 16:20:14 +0700

    view routes

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blog', function () {
-    return 'Hello World!';
-});
 
-Route::get('/call-name-variable', function () {
-    $a = 2;
-    $b = 3;
-    $c = $a + $b;
-    return 'Hasil dari variabel $c adalah '.$c;
-});
+// 1. basic routing
+// Route::get('/greeting', function () {
+//     return 'Hello World!';
+// });
+
+// Route::get('/call-name-variable', function () {
+//     $a = 2;
+//     $b = 3;
+//     $c = $a + $b;
+//     return 'Hasil dari variabel $c adalah '.$c;
+// });
+
+// 2. view routes
+// Route::get('/blog', function () {
+//    return view('blog');
+// });
+
+// opsi 2 view routes panggil digunakan variabel view tanpa 'get'
+// Route::view('/blog', 'blog');
+
+// opsi 3 view routes panggil digunakan variabel nama-nama (misalkan data atau bebas nama variabel akan dipanggil untuk menampilkan post view blog.blade.php) dengan 'get'
+// Route::get('/blog', function () {
+//     return view('blog', ['data' => 'saya programmer pemula']);
+// });
+
+// opsi 4 view routes panggil digunakan variabel view dan nama-nama (misalkan data atau bebas nama variabel akan dipanggil untuk menampilkan post view blog.blade.php) tanpa 'get'
+Route::view('blog', 'blog', ['data' => 'saya programmer pemula']);
