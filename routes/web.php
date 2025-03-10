330d9a8dc08c0fb9a5226647de0a8dd39d8e86a0
Tuahta Ramadhani <tuahtaramadhani@gmail.com>
2025-03-10 09:34:36 +0700

    named routes

--- routes/web.php
+++ routes/web.php
@@ -37,11 +37,11 @@ Route::get('/', function () {
 // Route::view('blog', 'blog', ['data' => 'saya programmer pemula']);
 
 // opsi 5 view routes panggil digunakan ambil data dari database
-Route::get('/blog', function () {
-    // ambil data dari database
-    $profile = 'aku programmer noob';
-    return view('blog', ['data' => $profile]);
-});
+// Route::get('/blog', function () {
+//     // ambil data dari database
+//     $profile = 'aku programmer noob';
+//     return view('blog', ['data' => $profile]);
+// });
 
 // 3. route parameter
 // Route::get('/blog/1', function () {
@@ -59,13 +59,26 @@ Route::get('/blog', function () {
 // });
 
 // opsi 4 route parameter panggil digunakan parameter variabel halaman 'ID' dan 'Title'
-Route::get('/blog/{id}/{title}', function ($id) {
-    return 'ini adalah blog '.$id;
-});
+// Route::get('/blog/{id}/{title}', function ($id) {
+//     return 'ini adalah blog '.$id;
+// });
 
 // opsi 5 route parameter panggil digunakan parameter variabel halaman 'ID atau Title' dengan http request
+// Route::get('/blog/{id}', function (Request $request) {
+//     // $name = $request -> name;
+//     // $password = $request -> password;
+//     return 'ini adalah blog '.$request->id;
+// });
+
+// 4. named routes
+// opsi 5 view routes panggil digunakan ambil data dari database
+Route::get('/blog', function () {
+    // ambil data dari database
+    $profile = 'aku programmer noob';
+    return view('blog', ['data' => $profile]);
+})->name('blog');
+
 Route::get('/blog/{id}', function (Request $request) {
-    // $name = $request -> name;
-    // $password = $request -> password;
-    return 'ini adalah blog '.$request->id;
+    // anggap aja melakukan update data & berhasil
+    return redirect()->route(('blog'));
 });
