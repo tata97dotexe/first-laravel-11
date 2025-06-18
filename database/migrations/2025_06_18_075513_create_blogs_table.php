665385f0538cfcb5a6ab1d18a60cb39043523627
Tuahta Ramadhani <tuahtaramadhani@gmail.com>
2025-06-18 14:58:07 +0700

    create table menggunakan migration

--- database/migrations/2025_06_18_075513_create_blogs_table.php
+++ database/migrations/2025_06_18_075513_create_blogs_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('example3', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('example3');
+        Schema::dropIfExists('blogs');
     }
 };
